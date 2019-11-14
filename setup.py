a2dcb0cb5b9bda3c92fc46ef0f1fadb307ea2506
Nathan Page <nathanrpage97@gmail.com>
2019-11-14 01:30:50 +0000

    feat: make the server installable with setuptools

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='liquid_crystal_server',
+    version='0.0.2',
+    author="Nathan Page",
+    author_email='nathanrpage97@gmail.com',
+    description='Pure python REST server to control an I2C LCD',
+    license='MIT',
+    url='https://github.com/nathanrpage97/liquid-crystal',
+    keywords='LCD display I2C server',
+    packages=find_packages()
+)
