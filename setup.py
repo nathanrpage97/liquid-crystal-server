ea1c433ae29b12656c05a869299c98084a58bc89
Nathan Page <nathanrpage97@gmail.com>
2019-11-16 17:33:46 -0500

    feat: new minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='liquid_crystal_server',
-    version='0.0.2',
+    version='0.0.3',
     author="Nathan Page",
     author_email='nathanrpage97@gmail.com',
     description='Pure python REST server to control an I2C LCD',
