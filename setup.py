7ba877f6cc2c3aa75d12da9847260b861fa7ecf8
Nathan Page <nathanrpage97@gmail.com>
2019-11-28 18:26:04 +0000

    feat: upgrade to using v0.1.0 liquid-crystal

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup, find_packages
 
 
-setup(
-    name='liquid_crystal_server',
-    version='0.0.3',
-    author="Nathan Page",
-    author_email='nathanrpage97@gmail.com',
-    description='Pure python REST server to control an I2C LCD',
-    license='MIT',
-    url='https://github.com/nathanrpage97/liquid-crystal',
-    keywords='LCD display I2C server',
-    packages=find_packages(),
-    install_requires=['aiohttp', 'dataclasses-json', 'aiohttp-cors', 'aiojobs', 'liquid-crystal @ https://github.com/nathanrpage97/liquid-crystal/archive/v0.0.1.zip'],
-)
+setup(name='liquid_crystal_server', version='0.0.3', author="Nathan Page", author_email='nathanrpage97@gmail.com',
+      description='Pure python REST server to control an I2C LCD', license='MIT',
+      url='https://github.com/nathanrpage97/liquid-crystal', keywords='LCD display I2C server',
+      packages=find_packages(),
+      install_requires=['aiohttp', 'dataclasses-json', 'aiohttp-cors', 'aiojobs',
+                        'liquid-crystal @ https://github.com/nathanrpage97/liquid-crystal/archive/v0.1.0.zip'],)
